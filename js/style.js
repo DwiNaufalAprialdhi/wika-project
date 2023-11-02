c0a3595ce0fa012771a26342fbbb77d2224c248e
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-02 17:39:52 +0700

    fix slider hot topic and add title kmtv

--- js/style.js
+++ js/style.js
@@ -1,10 +1,12 @@
 const overlay = document.querySelector('.overlay')
 const dropdownMenu = document.querySelector('.dropdownMenu')
 
+// Toggle Menu Overlay Mobile
 function navToggle() {
     overlay.classList.toggle('overlay_active')
 }
 
+// Dropdown
 function dropdownToggle(){
     console.log('masuk')
     dropdownMenu.classList.toggle('flex')
