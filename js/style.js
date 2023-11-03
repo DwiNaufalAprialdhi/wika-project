1d23ea1c3dd6c9ea305559d3a45a7dcb6aaa2619
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-03 09:54:34 +0700

    add slide Pertanyaan and KMTV section

--- js/style.js
+++ js/style.js
@@ -11,3 +11,115 @@ function dropdownToggle(){
     console.log('masuk')
     dropdownMenu.classList.toggle('flex')
 }
+
+ // Slider Hot Topic
+ var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      676: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // Slider Pertanyaan
+  var swiperQuestion = new Swiper(".mySwiperQuestion", {
+    spaceBetween: 0,
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+      },
+      768: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      676: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+    // Slider KMTV
+    var swiperKmtv = new Swiper(".mySwiperKmtv", {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1,
+        centeredSlides: false,
+        slidesPerGroupSkip: 1,
+        grabCursor: true,
+        keyboard: {
+          enabled: true,
+        },
+        breakpoints: {
+          992: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+          },
+          768: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+          676: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+        },
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
